f0b1fdfc54e5ecaa5038baa0c25f680f86b6ab67
Marius Wallin <post@mariuswallin.no>
2026-03-06 13:26:56 +0100

    feat: use components on routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Characters from "./components/Characters";
+import Character from "./components/Character";
 
 function App() {
- 
-
   return (
     <Routes>
-      <Route index element={<h1>Hjem</h1>} />
-      <Route path='characters' element={<h1>Karakterer</h1>} />
-      <Route path='characters/:id' element={<h1>Karakter</h1>} />
+      <Route index element={<Home />} />
+      <Route path="characters" element={<Characters />} />
+      <Route path="characters/:id" element={<Character />} />
     </Routes>
-
-  )
+  );
 }
 
-export default App
+export default App;
